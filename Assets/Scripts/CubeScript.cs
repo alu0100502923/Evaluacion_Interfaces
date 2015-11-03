e6965f86685370fdee62d4136c782489ca1a5ebe
alu0100502923 <alu0100502923@ull.edu.es>
2015-11-03 15:27:26 +0000

    Subiendo

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	Renderer rend;
+	int cont;
+	// Use this for initialization
+	void Start () {
+	
+		rend = GetComponent<Renderer>();
+		cont = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.Space)){
+			cont = Random.Range(1,5);
+			switch(cont){
+				case 1:
+					rend.material.color = Color.red;
+					break;
+				case 2:
+					rend.material.color = Color.yellow;
+					break;
+				case 3:
+					rend.material.color = Color.green;
+					break;
+				case 4:
+					rend.material.color = Color.blue;
+					break;
+					
+
+			}
+			 
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+
+	float speed ;
+	Rigidbody rb;
+	
+	// Use this for initialization
+	void Start () {
+		speed = 10;
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.transform.Translate(Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime, 0, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime);
+	}
+}
